3d75f06fe7e94aea59f958f799a5cd9fa2b82949
Marcin Wyszynski <marcin.pixie@gmail.com>
2015-09-29 10:57:19 +0200

    Have the report contain line range instead of just first line of violation

--- test/test_flay.rb
+++ test/test_flay.rb
@@ -222,8 +222,8 @@ class TestSexp < Minitest::Test
       Total score (lower is better) = 16
 
       1) Similar code found in :class (mass = 16)
-        (string):1
-        (string):6
+        (string):1..1
+        (string):6..1
     END
 
     assert_equal "", err
@@ -242,8 +242,8 @@ class TestSexp < Minitest::Test
       Total score (lower is better) = 16
 
       1) Similar code found in :class (mass = 16)
-        (string):1
-        (string):6
+        (string):1..1
+        (string):6..6
     END
 
     assert_equal exp, out.string.gsub(/\d+/, "N")
@@ -263,8 +263,8 @@ class TestSexp < Minitest::Test
       Total score (lower is better) = 16
 
       1) Similar code found in :class (mass = 16)
-        A: (string):1
-        B: (string):6
+        A: (string):1..1
+        B: (string):6..6
 
          ##
       A: # I am a dog.
@@ -305,8 +305,8 @@ class TestSexp < Minitest::Test
       Total score (lower is better) = 16
 
       1) Similar code found in :class (mass = 16)
-        A: (string):1
-        B: (string):6
+        A: (string):1..1
+        B: (string):6..6
 
       A: source code 1
       B: source code 6
