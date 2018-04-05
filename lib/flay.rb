17a5de3e00d50de483254840c748675f223c6be8
Ryan Davis <ryand@zenspider.com>
2018-04-05 14:59:08 -0800

    Prepped for release [git-p4: depot-paths = "//src/flay/dev/": change = 11635]

--- lib/flay.rb
+++ lib/flay.rb
@@ -17,7 +17,7 @@ class File
 end
 
 class Flay
-  VERSION = "2.10.0" # :nodoc:
+  VERSION = "2.11.0" # :nodoc:
 
   class Item < Struct.new(:structural_hash, :name, :bonus, :mass, :locations)
     alias identical? bonus
